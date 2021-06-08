fa85f1457152f9d2b41add2219071fb616711f7f
NamCheolKim <pkeowkd@naver.com>
2021-06-08 12:05:52 +0900

    .

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -10,7 +10,9 @@ class Command(BaseCommand):
         admin = User.objects.get_or_none(username="admin")
 
         if not admin:
-            User.objects.create_superuser("admin", "petsdiary0@gmail.com", "noel3907")
+            User.objects.create_superuser(
+                "admin@petsdiary.com", "petsdiary0@gmail.com", "noel3907"
+            )
             self.stdout.write(self.style.SUCCESS(f"SuperUser create"))
         else:
             self.stdout.write(self.style.SUCCESS(f"SuperUser Exist"))
